193a7db4ee6ef5f1fad192ecb557b6799024188e
Mtlewson <lewson@chapman.edu>
2022-04-06 20:06:47 -0700

    created HermitCrabUI
    able to upload and see videos

---
+++ OpenCVTest.py
@@ -0,0 +1,19 @@
+
+import cv2
+import numpy as np
+
+uploaded_vid = cv2.VideoCapture('Bob_and_aws.mov')
+
+while(uploaded_vid.isOpened()):
+    # capture frame by frame
+    ret, frame = uploaded_vid.read()
+    if ret == True:
+        cv2.imshow('frame', frame)
+        #press q to exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else :
+        break
+uploaded_vid.release()
+
+cv2.destroyAllWindows()
